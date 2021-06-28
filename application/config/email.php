a295db7657fcc3e12ff729163feda7f274cdcb64
mrtamvan <79129764+mrtamvan@users.noreply.github.com>
2021-06-29 06:58:53 +0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -2,10 +2,10 @@
 
 $config = [
   "protocol" => "smtp", // 'mail', 'sendmail', or 'smtp'
-  "smtp_host" => "mail.digicraft.id",
+  "smtp_host" => "", //mail server
   "smtp_port" => 465,
-  "smtp_user" => "info@digicraft.id",
-  "smtp_pass" => "Nopassword2907",
+  "smtp_user" => "",
+  "smtp_pass" => "",
   "smtp_crypto" => "ssl", //can be 'ssl' or 'tls' for example
   "mailtype" => "html", //plaintext 'text' mails or 'html'
   "smtp_timeout" => "4", //in seconds
